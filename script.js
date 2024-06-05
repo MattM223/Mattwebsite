707bfb939b0cb3f769480670d48a4df654e2e31f
MattM223 <mrm60@email.sc.edu>
2024-06-05 12:00:47 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
     const form = document.getElementById("request-a-consultation-form");
     const phoneInput = document.getElementById("phone");
     const emailInput = document.getElementById("email");
+    const zipCodeInput = document.getElementById("zipCode");
     form.addEventListener("submit", function (event) {
     // Initialize an array to store error messages
     const errors = [];
@@ -14,6 +15,11 @@ document.addEventListener("DOMContentLoaded", function () {
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     if (!emailRegex.test(emailInput.value)) {
     errors.push("Please enter a valid email address.");
+    }
+    // Validation for zip code all number and minimum length)
+    const zipNum = /^[0-9]+$/;
+    if (!zipNum.test(zipCodeInput.value.length < 5)) {
+    errors.push("Please enter a valid zip code.");
     }
     // If there are errors, prevent form submission and display them
     if (errors.length > 0) {
